bb136496ec43f013e8574d67b5ac3c40708274d2
deihnyx <deihnyx@gmail.com>
2021-03-23 13:19:39 -0500

    wip

---
+++ BruteForceHash.GUI/Main.Designer.cs
@@ -0,0 +1,414 @@
+﻿
+namespace BruteForceHash.GUI
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHexValues = new System.Windows.Forms.Label();
+            this.txtHexValues = new System.Windows.Forms.TextBox();
+            this.lblMethod = new System.Windows.Forms.Label();
+            this.cbMethod = new System.Windows.Forms.ComboBox();
+            this.lblPrefix = new System.Windows.Forms.Label();
+            this.txtPrefix = new System.Windows.Forms.TextBox();
+            this.lblDelimiter = new System.Windows.Forms.Label();
+            this.txtDelimiter = new System.Windows.Forms.TextBox();
+            this.chkSkipDigits = new System.Windows.Forms.CheckBox();
+            this.lblSkipDigits = new System.Windows.Forms.Label();
+            this.lblLowerCase = new System.Windows.Forms.Label();
+            this.chkLowerCase = new System.Windows.Forms.CheckBox();
+            this.lblNbThreads = new System.Windows.Forms.Label();
+            this.cbNbThreads = new System.Windows.Forms.ComboBox();
+            this.lblExcludePatterns = new System.Windows.Forms.Label();
+            this.txtIncludePatterns = new System.Windows.Forms.TextBox();
+            this.lblIncludePatterns = new System.Windows.Forms.Label();
+            this.txtExcludePatterns = new System.Windows.Forms.TextBox();
+            this.lblIncludeWord = new System.Windows.Forms.Label();
+            this.txtIncludeWord = new System.Windows.Forms.TextBox();
+            this.lblWordsLimit = new System.Windows.Forms.Label();
+            this.cbWordsLimit = new System.Windows.Forms.ComboBox();
+            this.lblDictionaries = new System.Windows.Forms.Label();
+            this.chklDictionaries = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // lblHexValues
+            // 
+            this.lblHexValues.AutoSize = true;
+            this.lblHexValues.Location = new System.Drawing.Point(16, 61);
+            this.lblHexValues.Name = "lblHexValues";
+            this.lblHexValues.Size = new System.Drawing.Size(75, 15);
+            this.lblHexValues.TabIndex = 0;
+            this.lblHexValues.Text = "Hex Value(s):";
+            // 
+            // txtHexValues
+            // 
+            this.txtHexValues.Location = new System.Drawing.Point(123, 58);
+            this.txtHexValues.Name = "txtHexValues";
+            this.txtHexValues.PlaceholderText = "0x105274ba4f";
+            this.txtHexValues.Size = new System.Drawing.Size(296, 23);
+            this.txtHexValues.TabIndex = 1;
+            // 
+            // lblMethod
+            // 
+            this.lblMethod.AutoSize = true;
+            this.lblMethod.Location = new System.Drawing.Point(16, 21);
+            this.lblMethod.Name = "lblMethod";
+            this.lblMethod.Size = new System.Drawing.Size(71, 15);
+            this.lblMethod.TabIndex = 2;
+            this.lblMethod.Text = "Attack Type:";
+            // 
+            // cbMethod
+            // 
+            this.cbMethod.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbMethod.FormattingEnabled = true;
+            this.cbMethod.Items.AddRange(new object[] {
+            "Dictionary",
+            "Letter"});
+            this.cbMethod.Location = new System.Drawing.Point(123, 18);
+            this.cbMethod.Name = "cbMethod";
+            this.cbMethod.Size = new System.Drawing.Size(114, 23);
+            this.cbMethod.TabIndex = 3;
+            // 
+            // lblPrefix
+            // 
+            this.lblPrefix.AutoSize = true;
+            this.lblPrefix.Location = new System.Drawing.Point(16, 101);
+            this.lblPrefix.Name = "lblPrefix";
+            this.lblPrefix.Size = new System.Drawing.Size(40, 15);
+            this.lblPrefix.TabIndex = 4;
+            this.lblPrefix.Text = "Prefix:";
+            // 
+            // txtPrefix
+            // 
+            this.txtPrefix.Location = new System.Drawing.Point(123, 98);
+            this.txtPrefix.Name = "txtPrefix";
+            this.txtPrefix.Size = new System.Drawing.Size(114, 23);
+            this.txtPrefix.TabIndex = 5;
+            // 
+            // lblDelimiter
+            // 
+            this.lblDelimiter.AutoSize = true;
+            this.lblDelimiter.Location = new System.Drawing.Point(250, 101);
+            this.lblDelimiter.Name = "lblDelimiter";
+            this.lblDelimiter.Size = new System.Drawing.Size(90, 15);
+            this.lblDelimiter.TabIndex = 6;
+            this.lblDelimiter.Text = "Word Delimiter:";
+            // 
+            // txtDelimiter
+            // 
+            this.txtDelimiter.Location = new System.Drawing.Point(382, 98);
+            this.txtDelimiter.Name = "txtDelimiter";
+            this.txtDelimiter.Size = new System.Drawing.Size(37, 23);
+            this.txtDelimiter.TabIndex = 7;
+            this.txtDelimiter.Text = "_";
+            this.txtDelimiter.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // chkSkipDigits
+            // 
+            this.chkSkipDigits.AutoSize = true;
+            this.chkSkipDigits.Location = new System.Drawing.Point(123, 141);
+            this.chkSkipDigits.Name = "chkSkipDigits";
+            this.chkSkipDigits.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.chkSkipDigits.Size = new System.Drawing.Size(15, 14);
+            this.chkSkipDigits.TabIndex = 8;
+            this.chkSkipDigits.UseVisualStyleBackColor = true;
+            // 
+            // lblSkipDigits
+            // 
+            this.lblSkipDigits.AutoSize = true;
+            this.lblSkipDigits.Location = new System.Drawing.Point(16, 140);
+            this.lblSkipDigits.Name = "lblSkipDigits";
+            this.lblSkipDigits.Size = new System.Drawing.Size(65, 15);
+            this.lblSkipDigits.TabIndex = 9;
+            this.lblSkipDigits.Text = "Skip Digits:";
+            // 
+            // lblLowerCase
+            // 
+            this.lblLowerCase.AutoSize = true;
+            this.lblLowerCase.Location = new System.Drawing.Point(250, 140);
+            this.lblLowerCase.Name = "lblLowerCase";
+            this.lblLowerCase.Size = new System.Drawing.Size(102, 15);
+            this.lblLowerCase.TabIndex = 10;
+            this.lblLowerCase.Text = "Force Lower Case:";
+            // 
+            // chkLowerCase
+            // 
+            this.chkLowerCase.AutoSize = true;
+            this.chkLowerCase.Checked = true;
+            this.chkLowerCase.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkLowerCase.Location = new System.Drawing.Point(387, 141);
+            this.chkLowerCase.Name = "chkLowerCase";
+            this.chkLowerCase.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.chkLowerCase.Size = new System.Drawing.Size(15, 14);
+            this.chkLowerCase.TabIndex = 11;
+            this.chkLowerCase.UseVisualStyleBackColor = true;
+            // 
+            // lblNbThreads
+            // 
+            this.lblNbThreads.AutoSize = true;
+            this.lblNbThreads.Location = new System.Drawing.Point(250, 21);
+            this.lblNbThreads.Name = "lblNbThreads";
+            this.lblNbThreads.Size = new System.Drawing.Size(51, 15);
+            this.lblNbThreads.TabIndex = 12;
+            this.lblNbThreads.Text = "Threads:";
+            // 
+            // cbNbThreads
+            // 
+            this.cbNbThreads.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbNbThreads.FormattingEnabled = true;
+            this.cbNbThreads.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50",
+            "51",
+            "52",
+            "53",
+            "54",
+            "55",
+            "56",
+            "57",
+            "58",
+            "59",
+            "60",
+            "61",
+            "62",
+            "63",
+            "64"});
+            this.cbNbThreads.Location = new System.Drawing.Point(328, 18);
+            this.cbNbThreads.Name = "cbNbThreads";
+            this.cbNbThreads.Size = new System.Drawing.Size(91, 23);
+            this.cbNbThreads.TabIndex = 13;
+            // 
+            // lblExcludePatterns
+            // 
+            this.lblExcludePatterns.AutoSize = true;
+            this.lblExcludePatterns.Location = new System.Drawing.Point(16, 181);
+            this.lblExcludePatterns.Name = "lblExcludePatterns";
+            this.lblExcludePatterns.Size = new System.Drawing.Size(97, 15);
+            this.lblExcludePatterns.TabIndex = 14;
+            this.lblExcludePatterns.Text = "Exclude Patterns:";
+            // 
+            // txtIncludePatterns
+            // 
+            this.txtIncludePatterns.Location = new System.Drawing.Point(123, 178);
+            this.txtIncludePatterns.Name = "txtIncludePatterns";
+            this.txtIncludePatterns.PlaceholderText = "{1}_{1},{2}_{2}";
+            this.txtIncludePatterns.Size = new System.Drawing.Size(296, 23);
+            this.txtIncludePatterns.TabIndex = 15;
+            // 
+            // lblIncludePatterns
+            // 
+            this.lblIncludePatterns.AutoSize = true;
+            this.lblIncludePatterns.Location = new System.Drawing.Point(16, 222);
+            this.lblIncludePatterns.Name = "lblIncludePatterns";
+            this.lblIncludePatterns.Size = new System.Drawing.Size(95, 15);
+            this.lblIncludePatterns.TabIndex = 16;
+            this.lblIncludePatterns.Text = "Include Patterns:";
+            // 
+            // txtExcludePatterns
+            // 
+            this.txtExcludePatterns.Location = new System.Drawing.Point(123, 219);
+            this.txtExcludePatterns.Name = "txtExcludePatterns";
+            this.txtExcludePatterns.PlaceholderText = "{3}";
+            this.txtExcludePatterns.Size = new System.Drawing.Size(296, 23);
+            this.txtExcludePatterns.TabIndex = 17;
+            // 
+            // lblIncludeWord
+            // 
+            this.lblIncludeWord.AutoSize = true;
+            this.lblIncludeWord.Location = new System.Drawing.Point(16, 264);
+            this.lblIncludeWord.Name = "lblIncludeWord";
+            this.lblIncludeWord.Size = new System.Drawing.Size(81, 15);
+            this.lblIncludeWord.TabIndex = 18;
+            this.lblIncludeWord.Text = "Include Word:";
+            // 
+            // txtIncludeWord
+            // 
+            this.txtIncludeWord.Location = new System.Drawing.Point(123, 261);
+            this.txtIncludeWord.Name = "txtIncludeWord";
+            this.txtIncludeWord.PlaceholderText = "mario";
+            this.txtIncludeWord.Size = new System.Drawing.Size(114, 23);
+            this.txtIncludeWord.TabIndex = 19;
+            // 
+            // lblWordsLimit
+            // 
+            this.lblWordsLimit.AutoSize = true;
+            this.lblWordsLimit.Location = new System.Drawing.Point(250, 264);
+            this.lblWordsLimit.Name = "lblWordsLimit";
+            this.lblWordsLimit.Size = new System.Drawing.Size(74, 15);
+            this.lblWordsLimit.TabIndex = 20;
+            this.lblWordsLimit.Text = "Words Limit:";
+            // 
+            // cbWordsLimit
+            // 
+            this.cbWordsLimit.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbWordsLimit.FormattingEnabled = true;
+            this.cbWordsLimit.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cbWordsLimit.Location = new System.Drawing.Point(328, 261);
+            this.cbWordsLimit.Name = "cbWordsLimit";
+            this.cbWordsLimit.Size = new System.Drawing.Size(91, 23);
+            this.cbWordsLimit.TabIndex = 21;
+            // 
+            // lblDictionaries
+            // 
+            this.lblDictionaries.AutoSize = true;
+            this.lblDictionaries.Location = new System.Drawing.Point(16, 302);
+            this.lblDictionaries.Name = "lblDictionaries";
+            this.lblDictionaries.Size = new System.Drawing.Size(72, 15);
+            this.lblDictionaries.TabIndex = 22;
+            this.lblDictionaries.Text = "Dictionaries:";
+            // 
+            // chklDictionaries
+            // 
+            this.chklDictionaries.FormattingEnabled = true;
+            this.chklDictionaries.Location = new System.Drawing.Point(123, 302);
+            this.chklDictionaries.Name = "chklDictionaries";
+            this.chklDictionaries.Size = new System.Drawing.Size(296, 112);
+            this.chklDictionaries.TabIndex = 23;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 604);
+            this.Controls.Add(this.chklDictionaries);
+            this.Controls.Add(this.lblDictionaries);
+            this.Controls.Add(this.cbWordsLimit);
+            this.Controls.Add(this.lblWordsLimit);
+            this.Controls.Add(this.txtIncludeWord);
+            this.Controls.Add(this.lblIncludeWord);
+            this.Controls.Add(this.txtExcludePatterns);
+            this.Controls.Add(this.lblIncludePatterns);
+            this.Controls.Add(this.txtIncludePatterns);
+            this.Controls.Add(this.lblExcludePatterns);
+            this.Controls.Add(this.cbNbThreads);
+            this.Controls.Add(this.lblNbThreads);
+            this.Controls.Add(this.chkLowerCase);
+            this.Controls.Add(this.lblLowerCase);
+            this.Controls.Add(this.lblSkipDigits);
+            this.Controls.Add(this.chkSkipDigits);
+            this.Controls.Add(this.txtDelimiter);
+            this.Controls.Add(this.lblDelimiter);
+            this.Controls.Add(this.txtPrefix);
+            this.Controls.Add(this.lblPrefix);
+            this.Controls.Add(this.cbMethod);
+            this.Controls.Add(this.lblMethod);
+            this.Controls.Add(this.txtHexValues);
+            this.Controls.Add(this.lblHexValues);
+            this.Name = "Main";
+            this.Text = "BruteForceHash";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHexValues;
+        private System.Windows.Forms.TextBox txtHexValues;
+        private System.Windows.Forms.Label lblMethod;
+        private System.Windows.Forms.ComboBox cbMethod;
+        private System.Windows.Forms.Label lblPrefix;
+        private System.Windows.Forms.TextBox txtPrefix;
+        private System.Windows.Forms.Label lblDelimiter;
+        private System.Windows.Forms.TextBox txtDelimiter;
+        private System.Windows.Forms.CheckBox chkSkipDigits;
+        private System.Windows.Forms.Label lblSkipDigits;
+        private System.Windows.Forms.Label lblLowerCase;
+        private System.Windows.Forms.CheckBox chkLowerCase;
+        private System.Windows.Forms.Label lblNbThreads;
+        private System.Windows.Forms.ComboBox cbNbThreads;
+        private System.Windows.Forms.Label lblExcludePatterns;
+        private System.Windows.Forms.TextBox txtIncludePatterns;
+        private System.Windows.Forms.Label lblIncludePatterns;
+        private System.Windows.Forms.TextBox txtExcludePatterns;
+        private System.Windows.Forms.Label lblIncludeWord;
+        private System.Windows.Forms.TextBox txtIncludeWord;
+        private System.Windows.Forms.Label lblWordsLimit;
+        private System.Windows.Forms.ComboBox cbWordsLimit;
+        private System.Windows.Forms.Label lblDictionaries;
+        private System.Windows.Forms.CheckedListBox chklDictionaries;
+    }
+}
+
---
+++ BruteForceHash.GUI/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BruteForceHash.GUI
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            cbMethod.SelectedIndex = 0;
+            cbNbThreads.SelectedIndex = 7;
+            cbWordsLimit.SelectedIndex = 2;
+
+            var allDictionaries = Directory.GetFiles("Dictionaries", "*.dic");
+            foreach (var dictionaryPath in allDictionaries)
+            {
+                chklDictionaries.Items.Add(Path.GetFileNameWithoutExtension(dictionaryPath));
+;            }
+        }
+    }
+}
