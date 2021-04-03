86cb30c71f0faa4f75dd12473a79469a3327ad3c
Deinonychus71 <deihnyx@gmail.com>
2021-04-02 22:33:53 -0500

    prep

---
+++ HashParser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HashParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = ""
+
+            var t = new ParamFile();
+            t.Open(inputFile);
+        }
+    }
+}
