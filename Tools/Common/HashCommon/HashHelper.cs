149f3b85d3a84af1c0a36bb789d07e637d3af2d2
Deinonychus71 <deihnyx@gmail.com>
2022-02-20 21:03:46 -0600

    cleanup

---
+++ Tools/Common/HashCommon/HashHelper.cs
@@ -0,0 +1,51 @@
+﻿using paracobNET;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace HashCommon
+{
+    public  class HashHelper
+    {
+        private Dictionary<ulong, string> _paramLabels;
+
+        public HashHelper(string inputFileParamLabels)
+        {
+            _paramLabels = GetParamLabels(inputFileParamLabels);
+        }
+
+        public ParamFile OpenParamFile(string inputFile)
+        {
+            var t = new ParamFile();
+            t.Open(inputFile);
+            return t;
+        }
+
+        public string GetHexaLabel(ulong hexValue)
+        {
+            if (hexValue == 0)
+                return string.Empty;
+
+            return _paramLabels.ContainsKey(hexValue) ? _paramLabels[hexValue] : $"0x{hexValue:x10}";
+        }
+
+        #region Static
+        public static Dictionary<ulong, string> GetParamLabels(string inputFileParamLabels)
+        {
+            var hashes = File.ReadAllLines(inputFileParamLabels);
+            var output = new Dictionary<ulong, string>();
+
+            foreach (var hash in hashes)
+            {
+                var hashSplit = hash.Split(",", StringSplitOptions.RemoveEmptyEntries);
+                if (hashSplit.Length > 2)
+                    throw new Exception("Bad ParamLabels");
+                if (hashSplit.Length == 2)
+                    output.Add(Convert.ToUInt64(hashSplit[0], 16), hashSplit[1]);
+            }
+
+            return output;
+        }
+        #endregion
+    }
+}
---
+++ Tools/Research/HashTable/Options.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace HashTable
+{
+    public class Options
+    {
+        [Option('i', "input_file", Required = false, Default = "root", HelpText = "Param to PRC file")]
+        public string InputFile { get; set; }
+
+        [Option('l', "input_list", Required = false, Default = "root", HelpText = "Param to input list")]
+        public string InputFileList { get; set; }
+
+        [Option('o', "output_file", Required = false, Default = "root_csv", HelpText = "Output param to root")]
+        public string OutputCSV { get; set; }
+
+        [Option('p', "input_param_labels", Required = false, Default = "ParamLabels.csv", HelpText = "Input ParamLabel")]
+        public string InputParamLabelsFile { get; set; }
+    }
+}
