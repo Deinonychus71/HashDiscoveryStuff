26b06d36afd8f7399f168e2f821d35563f12f18b
Deinonychus71 <deihnyx@gmail.com>
2022-02-20 21:09:22 -0600

    fix options

--- Tools/Research/HashTable/Options.cs
+++ Tools/Research/HashTable/Options.cs
@@ -4,13 +4,13 @@ namespace HashTable
 {
     public class Options
     {
-        [Option('i', "input_file", Required = false, Default = "root", HelpText = "Param to PRC file")]
+        [Option('i', "input_file", Required = true, HelpText = "Path to Param PRC file, for example 'ui_bgm_db.prc'")]
         public string InputFile { get; set; }
 
-        [Option('l', "input_list", Required = false, Default = "root", HelpText = "Param to input list")]
+        [Option('l', "input_list", Required = true, HelpText = "Param input list, for ex 'db_root'")]
         public string InputFileList { get; set; }
 
-        [Option('o', "output_file", Required = false, Default = "root_csv", HelpText = "Output param to root")]
+        [Option('o', "output_file", Required = false, Default = "output.csv", HelpText = "Output param to root")]
         public string OutputCSV { get; set; }
 
         [Option('p', "input_param_labels", Required = false, Default = "ParamLabels.csv", HelpText = "Input ParamLabel")]
